71473ab7ae3438d1c11203c6bda861ed2733e137
Lloyd McCarthy <lloydmccar@gmail.com>
2019-04-24 13:45:58 -0400

    Reverting semicolon remoal

--- src/googleAnalytics.ts
+++ src/googleAnalytics.ts
@@ -3,9 +3,16 @@ import { GaxiosResponse } from "gaxios";
 
 const viewId = process.env.VIEW_ID || 34065145;
 const scopes = "https://www.googleapis.com/auth/analytics.readonly";
-const jwt = new google.auth.JWT(process.env.CLIENT_EMAIL, "", process.env.PRIVATE_KEY, scopes);
+const jwt = new google.auth.JWT(
+    process.env.CLIENT_EMAIL,
+    "",
+    process.env.PRIVATE_KEY,
+    scopes
+);
 
-export default async function getData(studyName: string): Promise<GaxiosResponse> {
+export default async function getData(
+    studyName: string
+): Promise<GaxiosResponse> {
     const response = await jwt.authorize();
     return await google.analytics("v3").data.ga.get({
         auth: jwt,
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import * as querystring from "querystring"
-import * as url from "url"
-import { isArray } from "util"
-import getData from "./googleAnalytics"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as querystring from "querystring";
+import * as url from "url";
+import { isArray } from "util";
+import getData from "./googleAnalytics";
 
-const server = express()
-const port = process.env.PORT || 1337
+const server = express();
+const port = process.env.PORT || 1337;
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get("/", async (req, res, next) => {
     try {
-        const parsedURL = url.parse(req.url)
+        const parsedURL = url.parse(req.url);
         if (parsedURL.query) {
-            const parameters = querystring.parse(parsedURL.query)
-            console.log(`Queried study: ${parameters.studyId}`)
+            const parameters = querystring.parse(parsedURL.query);
+            console.log(`Queried study: ${parameters.studyId}`);
             if (!isArray(parameters.studyId)) {
-                const response = await getData(parameters.studyId)
-                res.json(response)
+                const response = await getData(parameters.studyId);
+                res.json(response);
             }
         }
     } catch (e) {
-        console.log(e)
-        next(e)
+        console.log(e);
+        next(e);
     }
-})
+});
 
 server.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on port ${port}`);
+});
